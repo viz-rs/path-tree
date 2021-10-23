aef77186fbdb7641f5d8a0a78e1c473de2bb8037
Fangdun Tsai <cfddream@gmail.com>
2021-10-24 01:43:55 +0800

    chore(test): random shuffle routes for github case

--- tests/github.rs
+++ tests/github.rs
@@ -1,4 +1,5 @@
 use path_tree::PathTree;
+use rand::seq::SliceRandom;
 
 #[allow(dead_code)]
 #[path = "fixtures/github.rs"]
@@ -8,14 +9,23 @@ use github::{ROUTES_URLS, ROUTES_WITH_COLON};
 
 #[test]
 fn github() {
+    let mut routes = ROUTES_WITH_COLON
+        .iter()
+        .zip(ROUTES_URLS.iter())
+        .map(|(a, b)| (*a, *b))
+        .collect::<Vec<_>>();
+
+    routes.shuffle(&mut rand::thread_rng());
+
     let mut tree: PathTree<usize> = PathTree::new();
-    for (i, r) in ROUTES_WITH_COLON.iter().enumerate() {
+
+    for (i, (r, ..)) in routes.iter().enumerate() {
         tree.insert(r, i);
     }
 
     // println!("tree: {:#?}", tree);
 
-    for (i, r) in ROUTES_URLS.iter().enumerate() {
+    for (i, (_, r)) in routes.iter().enumerate() {
         let n = tree.find(r).unwrap();
         assert_eq!(*n.0, i);
         // println!("route params: {:#?}", n.1);
