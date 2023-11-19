1db30e78e0853eaed9807fe2f81a35a0b0599f2f
Fangdun Tsai <cfddream@gmail.com>
2023-11-20 06:32:57 +0800

    chore(deps): update exmaples with hyper v1.0

--- examples/hello.rs
+++ examples/hello.rs
@@ -1,44 +1,47 @@
 #![allow(unused_must_use)]
 
-use std::{convert::Infallible, future::Future, pin::Pin, sync::Arc};
+use std::{convert::Infallible, future::Future, net::SocketAddr, pin::Pin, sync::Arc};
 
+use bytes::Bytes;
+use http_body_util::Full;
 use hyper::{
-    server::Server,
-    service::{make_service_fn, service_fn},
-    Body, Request, Response, StatusCode,
+    body::Incoming, server::conn::http1, service::service_fn, Request, Response, StatusCode,
 };
+use hyper_util::rt::TokioIo;
 use path_tree::PathTree;
+use tokio::net::TcpListener;
 
 static NOT_FOUND: &[u8] = b"Not Found";
 
 type Params = Vec<(String, String)>;
+type Body = Full<Bytes>;
 
 trait Handler: Send + Sync + 'static {
     fn call<'a>(
         &'a self,
-        req: Request<Body>,
+        req: Request<Incoming>,
     ) -> Pin<Box<dyn Future<Output = Response<Body>> + Send + 'a>>;
 }
 
 impl<F, R> Handler for F
 where
-    F: Send + Sync + 'static + Fn(Request<Body>) -> R,
-    R: Future<Output = Response<Body>> + Send + 'static,
+    F: Send + Sync + 'static + Fn(Request<Incoming>) -> R,
+    R: Future<Output = Response<Full<Bytes>>> + Send + 'static,
 {
     fn call<'a>(
         &'a self,
-        req: Request<Body>,
+        req: Request<Incoming>,
     ) -> Pin<Box<dyn Future<Output = Response<Body>> + Send + 'a>> {
         let fut = (self)(req);
         Box::pin(async move { fut.await })
     }
 }
 
-async fn index(_: Request<Body>) -> Response<Body> {
+async fn index(_: Request<Incoming>) -> Response<Body> {
     Response::new(Body::from("Hello, Web!"))
 }
 
-async fn hello_world(req: Request<Body>) -> Response<Body> {
+async fn hello_world(req: Request<Incoming>) -> Response<Body> {
     let params = req.extensions().get::<Params>().unwrap();
     let mut s = String::new();
     s.push_str("Hello, World!\n");
@@ -48,7 +51,7 @@ async fn hello_world(req: Request<Body>) -> Response<Body> {
     Response::new(Body::from(s))
 }
 
-async fn hello_user(req: Request<Body>) -> Response<Body> {
+async fn hello_user(req: Request<Incoming>) -> Response<Body> {
     let params = req.extensions().get::<Params>().unwrap();
     let mut s = String::new();
     s.push_str("Hello, ");
@@ -59,17 +62,19 @@ async fn hello_user(req: Request<Body>) -> Response<Body> {
     Response::new(Body::from(s))
 }
 
-async fn hello_rust(_: Request<Body>) -> Response<Body> {
+async fn hello_rust(_: Request<Incoming>) -> Response<Body> {
     Response::new(Body::from("Hello, Rust!"))
 }
 
-async fn login(_req: Request<Body>) -> Response<Body> {
+async fn login(_req: Request<Incoming>) -> Response<Body> {
     Response::new(Body::from("I'm logined!"))
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let addr = ([127, 0, 0, 1], 3000).into();
+    let addr: SocketAddr = ([127, 0, 0, 1], 3000).into();
+
+    let listener = TcpListener::bind(addr).await?;
 
     // /
     // ├── GET/ •0
@@ -87,40 +92,42 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     let tree = Arc::new(tree);
 
-    let make_service = make_service_fn(move |_| {
+    loop {
+        let (tcp, _) = listener.accept().await?;
+        let io = TokioIo::new(tcp);
         let router = Arc::clone(&tree);
 
-        async move {
-            Ok::<_, Infallible>(service_fn(move |mut req| {
-                let router = router.clone();
-                let path = "/".to_owned() + req.method().as_str() + req.uri().path();
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(
+                    io,
+                    service_fn(move |mut req| {
+                        let router = router.clone();
+                        let path = "/".to_owned() + req.method().as_str() + req.uri().path();
 
-                async move {
-                    Ok::<_, Infallible>(match router.find(&path) {
-                        Some((handler, route)) => {
-                            let p = route
-                                .params()
-                                .iter()
-                                .map(|p| (p.0.to_string(), p.1.to_string()))
-                                .collect::<Params>();
-                            req.extensions_mut().insert(p);
-                            handler.call(req).await
+                        async move {
+                            Ok::<_, Infallible>(match router.find(&path) {
+                                Some((handler, route)) => {
+                                    let p = route
+                                        .params()
+                                        .iter()
+                                        .map(|p| (p.0.to_string(), p.1.to_string()))
+                                        .collect::<Params>();
+                                    req.extensions_mut().insert(p);
+                                    handler.call(req).await
+                                }
+                                None => Response::builder()
+                                    .status(StatusCode::NOT_FOUND)
+                                    .body(NOT_FOUND.into())
+                                    .unwrap(),
+                            })
                         }
-                        None => Response::builder()
-                            .status(StatusCode::NOT_FOUND)
-                            .body(NOT_FOUND.into())
-                            .unwrap(),
-                    })
-                }
-            }))
-        }
-    });
-
-    let server = Server::bind(&addr).serve(make_service);
-
-    println!("Listening on http://{addr}");
-
-    server.await?;
-
-    Ok(())
+                    }),
+                )
+                .await
+            {
+                println!("Error serving connection: {:?}", err);
+            }
+        });
+    }
 }
